72697c1f00560ec644ce103ea80d48fcb5788c70
Daisuke Miyakawa <d.miyakawa@gmail.com>
2014-04-23 17:41:56 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+
+ * Check updates on Linux boxes. Includes CentOS and Ubuntu/Debian support.
+ * You need to install "update-notifier-common" on Debian machines.
+
+# License
+
+Apache2
---
+++ check_linux_updates.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from fabric.contrib.files import exists
+from fabric.api import hide,run
+from fabric.tasks import execute
+from fabric.state import env
+from fabric.utils import error
+
+# Prepare it yourself
+from hosts import HOSTS
+# HOSTS = ['localhost']
+
+def get_update_line(host, updates, sec_updates, reboot_required):
+    updates_str = '{}({})'.format(updates, sec_updates)
+    return ((u'{:<%d}: updates: {:>6}, reboot-required: {}' % env.host_column_size)
+            .format(host, updates_str, reboot_required))
+
+
+def _print_update_line(host, updates, sec_updates, reboot_required):
+    print(get_update_line(host, updates, sec_updates, reboot_required))
+
+
+def do_check_updates():
+    result = run('command -v apt-get >& /dev/null', quiet=True)
+    if result.succeeded:
+        # Ubuntu or Debian with additional apt-check
+        if not exists('/usr/lib/update-notifier/apt-check'):
+            print((u'{:<%d}: (apt-check is not available)' % env.host_column_size)
+                  .format(env.host))
+            return
+        result = run('/usr/lib/update-notifier/apt-check', quiet=True)
+        if result.failed:
+            error('{}: apt-check failed.'.format(env.host))
+            return
+        (updates, sec_updates) = map(lambda x: int(x), str(result.stdout).split(';'))
+        reboot_required = exists('/var/run/reboot-required')
+        if updates or sec_updates or reboot_required:
+            _print_update_line(env.host, updates, sec_updates, reboot_required)
+    else:
+        # CentOS
+        result = run('command -v yum >& /dev/null', quiet=True)
+        if result.failed:
+            error('{}: yum does not exist'.format(env.host))
+            return
+        result = run('yum --quiet check-update', quiet=True)
+        # yum returns 0 when there's no update and returns 100 when there are update(s).
+        # Will return 1 on error, but be a bit more pessimistic here.
+        if result.return_code != 0 and result.return_code != 100:
+            error('yum failed')
+            return
+        # Each line will contain actual package names to be updated.
+        # Just count the number of lines here.
+        # split('\n') will return [''] for an empty result. Filter it.
+        updates = len(filter(lambda x: x, str(result.stdout).split('\n')))
+        # It seems there's no way whether each is for security or not..
+        sec_updates = '?'
+
+        result_1 = run('rpm -q --last kernel', quiet=True)
+        result_2 = run('uname -r', quiet=True)
+        if result_1.succeeded and result_2.succeeded:
+            # e.g. "kernel-2.6.32-431.11.2.el6.x86_64"
+            latest_line = str(result_1.stdout).split()[0]
+            # e.g. "2.6.32-431.11.2.el6.x86_64"
+            current = str(result_2.stdout)
+            reboot_required = current not in latest_line
+        else:
+            reboot_required = '?'
+       
+        _print_update_line(env.host, updates, sec_updates, reboot_required)
+
+
+def main():
+    hosts = HOSTS
+    env.host_column_size = reduce(lambda x,y: max(x, len(y)), hosts, 0)
+    env.parallel = True
+    with hide('everything', 'status'):
+        execute(do_check_updates, hosts=hosts)
+
+
+if __name__ == '__main__':
+    main()
