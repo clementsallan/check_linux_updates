58604bae4da45c8765a7f155cc7a707437ac6434
Daisuke Miyakawa <d.miyakawa@gmail.com>
2014-04-24 12:30:27 +0900

    Support various stuff

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # What is this?
 
-Check updates on Linux boxes. Includes CentOS and Ubuntu/Debian support.
+ * Checks updates on Linux boxes.
+ * Has option for upgrading/rebooting each machine on demand.
+ * Can parallelize or serialize the execution.
+ * Supports CentOS and Ubuntu/Debian.
+ * No capability to select authenticatio scheme at this point
+     * You may need to enter password for each host when password-less sudo
+     is not allowed.
+ * "Grouping" capability exists.
 
 # Preparation
 
@@ -21,10 +28,18 @@ e.g. (hosts.py)
     test.example.com  : updates:   2(1), reboot-required: False
     mowa-net.jp       : updates:   5(0), reboot-required: False
     centos.mowa-net.jp: updates:  14(?), reboot-required: False
-
-* Shows updates(security-updates), reboot-required
-    * CentOS will show '?' for security-updates
-* "updates" and "reboot-required" will be aligned
+    > check-linux-updates.py --upgrade example.com
+    ..
+    (run upgrade command (apt-get dist-upgrade or yum upgrade)
+    ..
+    > check-linux-updates.py list_groups
+    example: example.com, test.example.com, test2.example.com
+    mowanet: mowa-net.jp, centos.mowa-net.jp, deb.mowa-net.jp
+    > check-linux-updates.py --upgrade-restart mowanet
+    ..
+    (check updates and upgrade commands to appropriate host in the group,
+     rebooting them if needed)
+    ..
 
 # License
 
--- check_linux_updates.py
+++ check_linux_updates.py
@@ -5,66 +5,106 @@ from __future__ import print_function
 import argparse
 
 from fabric.contrib.files import exists
-from fabric.api import hide,run
+from fabric.api import hide,sudo,run
 from fabric.tasks import execute
 from fabric.state import env
-from fabric.utils import abort,error
+from fabric.utils import abort,error,puts,warn
 
-# Prepare it yourself
-from hosts import HOSTS
-# HOSTS = ['localhost']
+import paramiko
+import socket
 
-def get_update_line(host, updates, sec_updates, reboot_required):
+# Prepare those function by yourself.
+from hosts import get_hosts, get_host_groups
+
+# http://stackoverflow.com/questions/1956777/
+def _is_host_up(host, port):
+    original_timeout = socket.getdefaulttimeout()
+    new_timeout = 3
+    socket.setdefaulttimeout(new_timeout)
+    try:
+        paramiko.Transport((host, port))
+        return True
+    except:
+        return False
+    finally:
+        socket.setdefaulttimeout(original_timeout)
+
+
+def _get_update_line(host, updates, sec_updates, reboot_required):
     updates_str = '{}({})'.format(updates, sec_updates)
-    return ((u'{:<%d}: updates: {:>6}, reboot-required: {}'
-             % env.host_column_size)
-            .format(host, updates_str, reboot_required))
+    ret = ((u'{:<%d}: {:>6}' % env.host_column_size)
+           .format(host, updates_str))
+    if reboot_required:
+        ret += ' (REBOOT-REQUIRED)'
+    return ret
 
 
 def _print_update_line(host, updates, sec_updates, reboot_required):
-    print(get_update_line(host, updates, sec_updates, reboot_required))
+    print(_get_update_line(host, updates, sec_updates, reboot_required))
 
 
 def check_debian_updates():
+    quiet = not env.args.verbose
+    if not _is_host_up(env.host, int(env.port)):
+        warn('Host {} on port {} is down.'.format(env.host, env.port))
+        return
+
     # Ubuntu or Debian with additional apt-check
     if not exists('/usr/lib/update-notifier/apt-check'):
         print((u'{:<%d}: (apt-check is not available)'
                % env.host_column_size)
               .format(env.host))
-        return
-    result = run('/usr/lib/update-notifier/apt-check', quiet=True)
+        return None
+    result = run('/usr/lib/update-notifier/apt-check',
+                 warn_only=True,
+                 quiet=quiet)
     if result.failed:
         error('{}: apt-check failed.'.format(env.host))
-        return
+        return None
     (updates, sec_updates) = map(lambda x: int(x),
                                  str(result.stdout).split(';'))
     reboot_required = exists('/var/run/reboot-required')
-    if updates or sec_updates or reboot_required:
+    if updates or sec_updates or reboot_required or env.args.verbose:
         _print_update_line(env.host, updates, sec_updates, reboot_required)
+
+    return (updates, sec_updates, reboot_required)
 
 
 def check_centos_updates():
+    quiet = not env.args.verbose
     # CentOS
-    result = run('command -v yum >& /dev/null', quiet=True)
+    result = run('command -v yum >& /dev/null', quiet=quiet)
     if result.failed:
         error('{}: yum does not exist'.format(env.host))
-        return
-    result = run('yum --quiet check-update', quiet=True)
+        return None
+    if quiet:
+        cmd = 'yum --quiet check-update'
+    else:
+        cmd = 'yum check-update'
+
+    # warn_only is not used because yum returns 100 even on successful cases,
+    # while warn_only treats it as error case, showing "Warning".
+    result = run(cmd, quiet=True)
+
     # yum returns 0 when there's no update and returns 100
     # when there are update(s).
     # Will return 1 on error, but be a bit more pessimistic here.
     if result.return_code != 0 and result.return_code != 100:
-        error('yum failed')
-        return
-    # Each line will contain actual package names to be updated.
-    # Just count the number of lines here.
-    # split('\n') will return [''] for an empty result. Filter it.
-    updates = len(filter(lambda x: x, str(result.stdout).split('\n')))
+        error('yum failed with return_code "{}"'.format(result.return_code))
+        return None
+
+    output = str(result.stdout)
+
+    # Count the number of lines that contain "update" at the end.
+    updates = len(filter(lambda x: x.rstrip().endswith('updates'),
+                         output.split('\n')))
+
+
     # It seems there's no way whether each is for security or not..
     sec_updates = '?'
 
-    result_1 = run('rpm -q --last kernel', quiet=True)
-    result_2 = run('uname -r', quiet=True)
+    result_1 = run('rpm -q --last kernel', quiet=quiet)
+    result_2 = run('uname -r', quiet=quiet)
     if result_1.succeeded and result_2.succeeded:
         # e.g. "kernel-2.6.32-431.11.2.el6.x86_64"
         latest_line = str(result_1.stdout).split()[0]
@@ -74,16 +114,44 @@ def check_centos_updates():
     else:
         reboot_required = '?'
    
-    if updates or reboot_required:
+    if updates or reboot_required or env.args.verbose:
         _print_update_line(env.host, updates, sec_updates, reboot_required)
+
+    return (updates, 0, reboot_required)
+
+
+def upgrade_debian():
+    quiet = not env.args.verbose
+    sudo('apt-get -y dist-upgrade', warn_only=True, quiet=quiet)
+
+def upgrade_centos():
+    quiet = not env.args.verbose
+    sudo('yum -y upgrade', warn_only=True, quiet=quiet)
 
 
 def do_check_updates():
+    quiet = not env.args.verbose
     result = run('command -v apt-get >& /dev/null', quiet=True)
-    if result.succeeded:
-        check_debian_updates()
+    is_debian = result.succeeded
+    
+    if is_debian:
+        result = check_debian_updates()
     else:
-        check_centos_updates()
+        result = check_centos_updates()
+
+    if result:
+        upgrade_done = False
+        (updates, sec_updates, reboot_required) = result
+        if (updates or sec_updates) and env.args.upgrade:
+            puts('Upgrading {}'.format(env.host))
+            if is_debian:
+                upgrade_debian()
+            else:
+                upgrade_centos()
+            upgrade_done = True
+        if (upgrade_done or reboot_required) and env.args.upgrade_restart:
+            puts('Rebooting {}'.format(env.host))
+            sudo('reboot', warn_only=True, quiet=quiet)
 
 
 def do_sanity_check():
@@ -93,13 +161,109 @@ def do_sanity_check():
 
 
 def main():
-    hosts = HOSTS
-    env.abort_on_prompts = True
-    env.host_column_size = reduce(lambda x,y: max(x, len(y)), hosts, 0)
-    env.parallel = True
-    with hide('everything', 'status'):
-        execute(do_sanity_check, hosts=hosts)
-    with hide('everything', 'status'):
+    parser = argparse.ArgumentParser(
+        description=u'Checks if remote hosts need update or not.')
+    parser.add_argument('-c', '--check',
+                        help=(u'Executes sanity check toward each host'
+                              u' serially (not in parallel).'
+                              u' If some hosts show prompt in the check phase,'
+                              u' this command will abort itself immediately.'
+                              u' Might be useful for "debugging" new hosts.'),
+                        action='store_true')
+    parser.add_argument('-s', '--serial',
+                        help=u'Executes check serially',
+                        action='store_true')
+    parser.add_argument('--upgrade',
+                        help=(u'Request hosts actually upgrade itself'
+                              u' when necessary.'
+                              u' Note that this will execute "dist-upgrade"'
+                              u' on debian(-like) OSes, not "upgrade.'),
+                        action='store_true')
+    parser.add_argument('--upgrade-restart',
+                        help=(u'Request hosts actually upgrade itself'
+                              u' and restart when update is available.'
+                              u' Note that, with this option, restart'
+                              u' will be executed regardless of the OS\'s'
+                              u' "Reboot-Required" status.'
+                              u' Note that this will execute "dist-upgrade"'
+                              u' on debian(-like) OSes, not "upgrade.'),
+                        action='store_true')
+    parser.add_argument('hosts', metavar='HOST',
+                        type=str,
+                        nargs='*',
+                        help=(u'Host names or host groups. If not specified,'
+                              u' default hosts configuration will be used.'
+                              u' This may allow "all" "list", "list_groups".'))
+    parser.add_argument('-q', '--quiet',
+                        action='store_true',
+                        help=u'Suppress unnecessary output.')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help=u'Show verbose outputs, including Fabric ones.')
+    args = parser.parse_args()        
+    output_groups = ()
+    if args.verbose:
+        output_groups = ()
+    elif args.quiet:
+        output_groups = ('everything', 'status')
+    else:
+        output_groups = ('running', 'status')
+
+    with hide(*output_groups):
+        if (args.upgrade or args.upgrade_restart) and not args.hosts:
+            abort(u'--upgrade/--upgrade-restart toward all hosts not allowed'
+                  u' by default.'
+                  u' Consider using "all" for host, forcing what you want.')
+
+        if args.hosts:
+            groups = get_host_groups()
+            if len(args.hosts) == 1 and args.hosts[0] == 'all':
+                hosts = get_hosts()
+            elif len(args.hosts) == 1 and args.hosts[0] == 'list':
+                print('\n'.join(get_hosts()))
+                return
+            elif len(args.hosts) == 1 and args.hosts[0] == 'list_groups':
+                column_size = reduce(lambda x,y: max(x, len(y)),
+                                     groups.keys(), 0)
+                for key, value in groups.iteritems():
+                    print((u'{:<%d}: {}' % column_size)
+                          .format(key, ', '.join(value)))
+                return
+
+            hosts = []
+            for host in args.hosts:
+                if groups.has_key(host):
+                    hosts.extend(groups[host])
+                else:
+                    hosts.append(host)
+        else:
+            hosts = get_hosts()
+        if not hosts:
+            abort('No hosts provided.')
+
+        # Determine left-most column size.
+        # It should be same as length of host name with maximum characters.
+        # e.g.
+        # ['mn.to', 'mowa-net.jp', 'test.mowa-net.jp']
+        # -> 17 (== len('test.mowa-net.jp'))
+        env.host_column_size = reduce(lambda x,y: max(x, len(y)), hosts, 0)
+
+        if args.upgrade_restart:
+            args.upgrade = True
+
+        # On serial execution there's no need to abort on prompts.
+        # Also assume serial execution when there's just one host.
+        if len(hosts) == 1:
+            args.serial = True
+        env.parallel = not args.serial
+        env.abort_on_prompts = not args.serial
+
+        # Remember our args.
+        env.args = args
+
+        if args.check:
+            puts('Start sanity check')
+            execute(do_sanity_check, hosts=hosts)
         execute(do_check_updates, hosts=hosts)
 
 
